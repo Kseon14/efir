ddd0d39a3facfb60456b67c454731580e7478a31
nbeztsinna <51158924+nbeztsinna@users.noreply.github.com>
2019-06-04 16:25:12 +0300

    Ui 2 (#7)
    * adding button for adding New Worker
    
    * change IP
    
    * create simple modal window for 'Worker'

--- client/src/views/Worker.vue
+++ client/src/views/Worker.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="workers">
     <h3>Workers</h3>
-    <div id="addWorker">
-      <button v-on:click="showModal = true">NEW WORKER</button>
-    </div>
+    <section id="addWorker">
+      <button @click="modal = !modal">Add</button>
+      <br>
+      <div v-if="modal" class="modal-bg">
+        <div class="modal-win">
+          <h3>New Worker</h3>
+          <div >
+            <div>
+              <label class="mdl-textfield__label" for="new-item-firstName">First Name</label>
+              <input class="mdl-textfield__input" type="text" id="new-item-firstName" v-model="newWorker.firstName">
+            </div>
+            <div>
+              <label class="mdl-textfield__label" for="new-item-lastName">Last Name</label>
+              <input class="mdl-textfield__input" type="text" id="new-item-lastName" v-model="newWorker.lastName">
+            </div>
+            <div>
+              <label class="mdl-textfield__label" for="new-item-salary">Base salary</label>
+              <input class="mdl-textfield__input" type="number" id="new-item-salary" v-model.number="newWorker.baseSalary">
+            </div>
+          </div>
+          <section>
+          <button @click="modal = !modal">Close</button>
+          <button id="new-item-add" @click="createWorker">Add</button>
+            <p>{{newWorker}}</p>
+          </section>
+        </div>
+      </div>
 
-    <br>
+    </section>
       <table id="firstTable" class="worker">
         <thead>
         <tr>
@@ -13,6 +37,7 @@
           <th>Name</th>
           <th>Base salary</th>
           <th>Status</th>
+          <th>Actions</th>
         </tr>
         </thead>
         <tbody>
@@ -21,10 +46,12 @@
           <td>{{row.lastName}} {{row.firstName}}</td>
           <td>{{row.baseSalary}}</td>
           <td>{{row.status}}</td>
+          <th><button @click="deleteWorker(row.id)">del</button></th>
         </tr>
         </tbody>
       </table>
     </div>
+
 </template>
 
 <script lang="ts">
@@ -32,11 +59,11 @@
   import axios from 'axios';
 
   export interface Worker {
-  id: number;
-  firstName: string;
-  lastName: string;
-  baseSalary: number;
-  status: string;
+  id ?: number;
+  firstName ?: string;
+  lastName ?: string;
+  baseSalary ?: number;
+  status ?: string;
 }
 
 @Component({
@@ -46,22 +73,48 @@
 
 export default class Workers extends Vue {
   public workers: Worker[] = [];
+  public newWorker:Worker = {
+    status: 'ACTIVE'
+  };
 
-  private async created() {
+  private async getWorkers(){
     const response = await axios.get('/api/workers');
     this.workers = await response.data;
   }
+  // private validateName() {
+  // if (!this.newWorker.firstName.length) {
+  // return (#new-item-add).disabled = true;
+  // }
+  // };
+  private async created() {
+    this.getWorkers()
+  }
+  private createWorker(){
+    axios.post('/api/workers', this.newWorker);
+    this.getWorkers();
+    this.modal = false
 }
+private deleteWorker(idUser:any){
+  axios.delete('/api/workers/'+idUser);
+  this.getWorkers();
+}
+
+  public modal: boolean = false;
+
+}
+
+
 </script>
 <style>
   button {
     background-color: white;
+    border-radius: .25rem;
     color: black;
+    margin-bottom: 2em;
     border: 2px solid #008CBA;
-    padding: 16px 32px;
-    margin: 4px 2px;
+    padding: 10px 30px;
     text-align: center;
-    font-size: 16px;
+    font-size: 13px;
     text-decoration: none;
     display: inline-block;
     transition-duration: 0.4s;
@@ -69,6 +122,24 @@ export default class Workers extends Vue {
   button:hover {
     background-color: #008CBA;
     color: white;
+  }
+  .modal-bg{
+    /*background-color: rgba(0,0,0, 0.5);*/
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .modal-win{
+    border: 1px lightslategrey solid;
+    background-color: white;
+    border-radius: .25rem;
+    padding: 1rem;
+    width: 33.33333%;
   }
   table.worker {
     font-family: 'Open Sans', sans-serif;
@@ -86,11 +157,24 @@ export default class Workers extends Vue {
 
   }
 
-  table.worker td {
+  table.worker td{
     padding: 5px;
     border-bottom: 1px solid rgba(170, 179, 232, 0.17);
     min-width: 20px;
     text-align: left;
+  }
+
+
+
+  .mdl-textfield__input{
+    border: 0;
+    border-bottom: 1px solid rgba(170, 179, 232, 0.17);
+    min-width: 20px;
+    padding: 5px;
+
+  }
+  input:focus{
+    outline: none;
   }
 
 </style>
