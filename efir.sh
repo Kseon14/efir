d4558ce77f93a7a6bfcfb0a4966cb07a25c4f00a
Alex Mamedov <Letter_can_walk@yahoo.com>
2020-02-04 19:34:05 +0200

    Update efir.sh

--- efir.sh
+++ efir.sh
@@ -1,42 +1,5 @@
 SERVICE_NAME=efir
 PATH_TO_JAR=/opt/$SERVICE_NAME/backend-0.0.1-SNAPSHOT.jar
 JAVA_PATH=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/jre/bin/java
-PID_PATH_NAME=/tmp/$SERVICE_NAME-pid
-case $1 in
-    start)
-        echo "Starting $SERVICE_NAME ..."
-        if [ ! -f $PID_PATH_NAME ]; then
-            nohup $JAVA_PATH -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-            echo "$SERVICE_NAME is already running ..."
-        fi
-    ;;
-    stop)
-        if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stoping ..."
-            kill $PID;
-            echo "$SERVICE_NAME stopped ..."
-            rm $PID_PATH_NAME
-        else
-            echo "$SERVICE_NAME is not running ..."
-        fi
-    ;;
-    restart)
-        if [ -f $PID_PATH_NAME ]; then
-            PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stopping ...";
-            kill $PID;
-            echo "$SERVICE_NAME stopped ...";
-            rm $PID_PATH_NAME
-            echo "$SERVICE_NAME starting ..."
-            nohup $JAVA_PATH -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-            echo "$SERVICE_NAME is not running ..."
-        fi
-    ;;
-esac
+
+$JAVA_PATH -jar $PATH_TO_JAR
