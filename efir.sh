983ebee185a6c81d597e1df1f9627f0ecc5a27a5
Alex Mamedov <Letter_can_walk@yahoo.com>
2020-02-04 14:22:50 +0200

    Update efir.sh

--- efir.sh
+++ efir.sh
@@ -6,8 +6,7 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup $JAVA_PATH -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup $JAVA_PATH -jar $PATH_TO_JAR &
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
@@ -32,8 +31,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup $JAVA_PATH -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup $JAVA_PATH -jar $PATH_TO_JAR &
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
